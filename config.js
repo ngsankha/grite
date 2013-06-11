240ca28d6a5a41c8beda81d7c9cd10a53884c7f4
Sankha Narayan Guria <sankha93@gmail.com>
2013-06-12 02:37:06 +0530

    added guest posts

--- config.js
+++ config.js
@@ -5,7 +5,9 @@ var config = {
             { text: 'Documentation',
               link: 'https://github.com/sankha93/grite/wiki/Documentation'} ],
   posts: [ '5711471',
-           '5711492'],
+           '5711492',
+           '5616961',
+           '5760270'],
   atOnce: 5,
   footerText: "Grite has been made with love by Sankha. Follow him on Twitter at <a href=\"https://twitter.com/sankha93\">@sankha93</a>."
 };
